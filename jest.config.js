b3663392f027d821388c9f32f48fbf056cf8e638
Vasco Singh <vasco@realignrx.com>
2024-12-01 22:40:16 -0600

    Resolving main.yml test suite errors

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  // ... other config options ...
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     'src/pages/Group.tsx',  // Exclude Group.tsx
